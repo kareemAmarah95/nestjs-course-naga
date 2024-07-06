121655371b065289dfc286c9087e71c42d553d4c
Kareem Amara <kareemamarah2356@gmail.com>
2024-07-06 17:10:55 +0300

    auth guard

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,11 +3,12 @@ import { ProductsModule } from './products/products.module';
 import { ChatModule } from './chat/chat.module';
 import { PostsModule } from './posts/posts.module';
 import { Module } from '@nestjs/common';
+import { DashboardModule } from './dashboard/dashboard.module';
 
 @Module({
   controllers: [],
   providers: [],
   exports: [],
-  imports: [UsersModule, ProductsModule, ChatModule, PostsModule],
+  imports: [UsersModule, ProductsModule, ChatModule, PostsModule, DashboardModule],
 })
 export class AppModule {}
